79313754636b06a133226bb1e28c1ab55f99798f
T.GNANESWAR <106004033+Gnaneswartunga@users.noreply.github.com>
2024-03-13 18:09:51 +0530

    Arraysum

--- Arraysum.java
+++ Arraysum.java
@@ -1,5 +1,5 @@
 //Sure, here's a Java program to find the sum of numbers in an array that are divisible by both 5 and 2:
-public class Main {
+public class Arraysum {
     public static void main(String[] args) 
     {
         int arrayvalues[] = {100, 52, 43, 99, 42, 40, 91};

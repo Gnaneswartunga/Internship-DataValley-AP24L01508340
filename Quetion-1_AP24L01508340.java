10ba0b959c6e29135797a39d053780607145fa7e
T.GNANESWAR <106004033+Gnaneswartunga@users.noreply.github.com>
2024-03-13 18:08:37 +0530

    Update Quetion-1_AP24L01508340.java

--- Quetion-1_AP24L01508340.java
+++ Quetion-1_AP24L01508340.java
@@ -1,3 +1,4 @@
+//Sure, here's a Java program to find the sum of numbers in an array that are divisible by both 5 and 2:
 public class Main {
     public static void main(String[] args) 
     {
@@ -7,7 +8,7 @@ public class Main {
         {
             if (number%5 == 0 && number%2 == 0) 
             {
-                sum = sum+number;
+                sum = sum + number;
             }
         } 
         System.out.println("Sum of numbers divisible by both 5 and 2: " + sum);

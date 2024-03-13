8026f79f52b843c46f8bf0e85a6d0af1aab65d76
T.GNANESWAR <106004033+Gnaneswartunga@users.noreply.github.com>
2024-03-13 18:03:11 +0530

    Update Quetion-1_AP24L01508340.java

--- Quetion-1_AP24L01508340.java
+++ Quetion-1_AP24L01508340.java
@@ -1,13 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-        int[] arrayvalues = {100, 52, 43, 99, 42, 40, 91};
+    public static void main(String[] args) 
+    {
+        int arrayvalues[] = {100, 52, 43, 99, 42, 40, 91};
         int sum = 0;
-        for (int number : values) {
-            if (number % 5 == 0 && number % 2 == 0) {
+        for (int number : values) 
+        {
+            if (number % 5 == 0 && number % 2 == 0) 
+            {
                 sum = sum+number;
             }
-        }
-        
+        } 
         System.out.println("Sum of numbers divisible by both 5 and 2: " + sum);
     }
 }

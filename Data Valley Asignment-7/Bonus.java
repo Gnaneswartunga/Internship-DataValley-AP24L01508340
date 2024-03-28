0bd42229b9768ad0fdd37c6ebbfce6b1e8dde01b
T.GNANESWAR <106004033+Gnaneswartunga@users.noreply.github.com>
2024-03-28 18:50:26 +0530

    Update Bonus.java

--- Data Valley Asignment-7/Bonus.java
+++ Data Valley Asignment-7/Bonus.java
@@ -3,18 +3,22 @@ package com.datavalley;
 import java.util.Scanner;
 
 public class Bonus {
-
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter No.Of.Years Organization Service: ");
-		int years = scan.nextInt();
-		if(years>5) {
-			System.out.println("Bonus added 5000");
-		}else if(years>=3 || years>=5) {
-			System.out.println("Bonus added 3000");
-		}else {
-			System.out.println("No Bonus");
+		System.out.print("Enter the current year: ");
+		int currentYear = scan.nextInt();
+		System.out.print("Enter the year of joining: ");
+		int joinedYear = scan.nextInt();
+		int serviceYears = currentYear - joinedYear;
+		int bonus = 0;
+		if (serviceYears > 5) {
+			bonus = 5000;
+		} else if (serviceYears >= 3 && serviceYears <= 5) {
+			bonus = 3000;
 		}
-		scan.close();
+		if (bonus > 0) {
+			System.out.println("Bonus of Rs. " + bonus + "/- awarded.");
+		} else {
+		}
 	}
 }
